06ea7b3237a1dbf021e3f0e62be6291af0cbd993
[firstname <zoro.ahmed68555@gmail.com>
2025-04-05 13:54:27 +0200

    added webanalytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,7 @@ createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <App />
       <ReactQueryDevtools />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 );
